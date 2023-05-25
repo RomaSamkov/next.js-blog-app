4ffdb6ab37e9c2b0bee73a139b4e6854586a01d0
RomaSamkov <Romansamkov.rs@gmail.com>
2023-05-25 14:09:51 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="px-6 mx-auto bg-slate-800">
-      <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
+    <main className="px-6 mx-auto">
+      <p className="mt-12 mb-12 text-3xl text-center text-white">
         Hello and Welcome 👋&nbsp;
         <span className="whitespace-nowrap">
           I&apos;m <span className="font-bold">Roma RS</span>.
