5e01f523e59b47a188a24d0228d9071a755b51b2
RomaSamkov <Romansamkov.rs@gmail.com>
2023-05-25 16:17:27 +0300

    Update NavBar.tsx

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,15 +1,19 @@
 import Link from "next/link";
+import { FaYoutube, FaTwitter, FaGithub, FaLaptop } from "react-icons/fa";
 
 export default function NavBar() {
   return (
     <nav className="bg-slate-600 p-4 sticky top-0 drop-shadow-xl z-10">
       <div className="prose prose-xl mx-auto flex justify-between flex-col sm:flex-row">
-        {/* <h1 className="text-3xl font-bold text-white grid place-content-center mb-2 md:mb-0"> */}
-        <Link href="/" className="text-white/90 no-underline hover:text-white">
-          Roma RS
-        </Link>
-        {/* </h1> */}
-        {/* <div className="flex flex-row justify-center sm:justify-evenly align-middle gap-4 text-white text-4xl lg:text-5xl">
+        <h1 className="text-3xl font-bold text-white grid place-content-center mb-2 md:mb-0">
+          <Link
+            href="/"
+            className="text-white/90 no-underline hover:text-white"
+          >
+            Roma RS
+          </Link>
+        </h1>
+        <div className="flex flex-row justify-center sm:justify-evenly align-middle gap-4 text-white text-4xl lg:text-5xl">
           <Link
             className="text-white/90 hover:text-white"
             href="https://www.youtube.com/@DaveGrayTeachesCode"
@@ -34,7 +38,7 @@ export default function NavBar() {
           >
             <FaTwitter />
           </Link>
-        </div> */}
+        </div>
       </div>
     </nav>
   );
